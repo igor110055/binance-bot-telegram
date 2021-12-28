00a4533ff6e411bfd6206a210973ebcc717abd9b
manoj nilanga <manojnilangadesilva@gmail.com>
2021-12-29 00:11:14 +0530

    testing api calls

--- test_trade.py
+++ test_trade.py
@@ -7,7 +7,9 @@ from binance.client import Client
 
 api_key = 'wFqCyHdnaygcjy4W4refvi6Jo4qdlLb0OUOIeWjcCUZ5F53s7DM05ShS7Kzv9YM7'
 api_secret = '4gmQwHlieQwQGHuHdsN7KILZWCxQdLXutTaOCjy7wwOi7QddRVebNueaEpeSETIj'
-
+LEVERAGE = 10
+SYMBOL = "OCEANUSDT"
+PERCENTAGE = 1
 
 # #testnet
 # api_key = "KZexYHU1F0a0tfcw9lpDWqSLnz1T3O9Idr4xUmw20oWG7u8HW9yBszL5ELFf0Z2h"
@@ -19,15 +21,37 @@ client = Client(api_key, api_secret)
 
 # print(client.futures_coin_create_order("BTCUSDT"))
 
-print(client.futures_get_open_orders())
 
-## working fine
-print(client.futures_change_leverage(symbol="BTCUSDT", leverage=5))
-print(client.futures_change_leverage(symbol="BTCUSDT", leverage=20))
-print(client.futures_create_order(symbol="BTCUSDT", side='BUY', type='MARKET', quantity=0.001))
+# ## working fine
+# print(client.futures_change_leverage(symbol="BTCUSDT", leverage=5))
+# print(client.futures_change_leverage(symbol="BTCUSDT", leverage=20))
+# print(client.futures_create_order(symbol="BTCUSDT", side='BUY', type='MARKET', quantity=0.001))
 
 
-print(client.futures_get_open_orders())
+
+# print(client.futures_change_leverage(symbol=SYMBOL, leverage=LEVERAGE))
+
+cheapest_price = float(client.futures_mark_price(symbol=SYMBOL)["markPrice"])
+print("cheapest_price: "+str(cheapest_price))
+
+usdt_balance = 0
+for asset in client.futures_account()["assets"]:
+    if(asset["asset"]=="USDT"):
+        usdt_balance = asset["availableBalance"]
+
+print("usdt_balance: "+usdt_balance)
+
+# print(client.futures_create_order(symbol=SYMBOL, side='BUY', type='LIMIT',price=cheapest_price+1, quantity=30))
+print(client.futures_create_order(symbol=SYMBOL, side='BUY', type='MARKET', quantity=30))
+
+
+# print(client.futures_get_open_orders())
+# print(client.futures_get_order(symbol='BTCUSDT'))
+# print(client.futures_get_all_orders())
+# print(client.futures_account())
+# print(client.futures_cancel_orders(symbol="BTCUSDT"))
+# print(client.futures_coin_account_balance())
+# print(client.futures_coin_get_all_orders())
 # print(client.futures_account_balance())
 
 
@@ -37,9 +61,6 @@ print(client.futures_get_open_orders())
 # print(client.futures_coin_get_all_orders(symbol="BTCUSDT"))
 
 # print(client.futures_cancel_orders(symbol="BTCUSDT"))
-
-
-
 
 
 
